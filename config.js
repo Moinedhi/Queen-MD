9b44bbdd74be6862c22c6301773f29e8032f88fc
Moinedhi <104745408+Moinedhi@users.noreply.github.com>
2023-01-31 12:16:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.session = process.env.SESSION_ID || 'RG5HUEp3TjY=',
+global.ownername = process.env.OWNER_NAME === undefined ? "Edhi" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '03112836299' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
